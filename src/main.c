8aca8e68c2f1b41cadcbb90867619b9de4c16294
Dánjal Leitisstein Olsen <danjal.olsen04@gmail.com>
2025-10-30 07:57:47 +0100

    Token IDs and successors implemented

--- src/main.c
+++ src/main.c
@@ -1,4 +1,14 @@
-// #include <ctype.h>
+/**
+ * @file main.c
+ * @author Dánjal Leitisstein Olsen (s255821@dtu.dk)
+ * @brief
+ * @version 0.1
+ * @date 2025-10-28
+ *
+ * @copyright Copyright (c) 2025
+ */
+
+#include <ctype.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,6 +17,7 @@
 
 #define MAX_WORD_COUNT 50'000
 #define MAX_SUCCESSOR_COUNT MAX_WORD_COUNT / 10
+#define RAND_MAX 50'000
 
 char book[] = {
 #embed "Frankenstein.txt"
@@ -33,37 +44,82 @@ size_t succs_sizes[MAX_WORD_COUNT];
 /// `"\xefthe" != "the"` even both print `the`.
 void replace_non_printable_chars_with_space() {
   // YOUR CODE HERE
+
+  for (size_t i = 0; i < strlen(book) - 1; i++) {
+    // If book[i] is not one of the ASCII printable characters,
+    // replace it with a space.
+    if (!isprint(book[i])) {
+      book[i] = ' ';
+    }
+  }
 }
 
 /// Returns token id if token is known, otherwise creates a new entry and
 /// returns the new token id
 size_t token_id(char *token) {
-  // YOUR CODE HERE
-
-  return 0;
+  size_t id;
+  for (id = 0; id < tokens_size; ++id) {
+    if (strcmp(tokens[id], token) == 0) {
+      return id;
+    }
+  }
+  tokens[id] = token;
+  tokens_size++;
+  return id;
 }
 
 /// Appends a token to the successors list of a `token`
 void append_to_succs(char *token, char *succ) {
-  // YOUR CODE HERE
+  auto next_empty_index_ptr = &succs_sizes[token_id(token)];
+
+  if (*next_empty_index_ptr >= MAX_SUCCESSOR_COUNT) {
+    printf("Successor array full.");
+    exit(EXIT_FAILURE);
+  }
+
+  succs[token_id(token)][(*next_empty_index_ptr)++] = succ;
 }
 
 /// Creates tokens from the `book` and fills `tokens` and `succs` arrays using
 /// the `token`s.
 void tokenize_and_fill_succs(char *delimiters, char *str) {
   // YOUR CODE HERE
+
+  // Block for tokenizing Frankenstein.txt and assigning token IDs.
+
+  char *token;
+  size_t index = 0;
+
+  token = strtok(str, delimiters);
+
+  while (token) {
+    tokens[index] = token;
+
+    token = strtok(NULL, delimiters);
+
+    index++;
+  }
+
+  // Block for populating successor table.
 }
 
 /// Returns last character of a string
 char last_char(char *str) {
   // YOUR CODE HERE
 
-  return 0;
+  char lastChar = str[strlen(str) - 1];
+
+  return lastChar;
 }
 
 /// Returns whether the token ends with `!`, `?` or `.`.
 bool token_ends_a_sentence(char *token) {
   // YOUR CODE HERE
+
+  if (!(last_char(token) == '!' || last_char(token) == '?' ||
+        last_char(token) == '.')) {
+    return false;
+  }
 
   return true;
 }
@@ -74,7 +130,20 @@ bool token_ends_a_sentence(char *token) {
 size_t random_token_id_that_starts_a_sentence() {
   // YOUR CODE HERE
 
-  return 0;
+  size_t tokenID = 0;
+  size_t randomTokenID;
+  bool foundID = false;
+
+  while (!foundID) {
+    // Generate a random tokenID from 0..tokens_size - 1.
+    randomTokenID = rand() % tokens_size;
+    if (isupper(tokens[randomTokenID][0])) {
+      tokenID = randomTokenID;
+      foundID = true;
+    }
+  }
+
+  return tokenID;
 }
 
 /// Generates a random sentence using `tokens`, `succs`, and `succs_sizes`.
